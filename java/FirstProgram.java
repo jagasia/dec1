d3a1249f246561211f9f849ed00226fab65cdb50
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2022-12-13 11:42:11 +0530

    11:42

---
+++ java/FirstProgram.java
@@ -0,0 +1,18 @@
+public class FirstProgram
+{
+	public static void main(String []args)
+	{
+		//System.out.println("Hello world");
+		byte marks=127;
+		++marks;
+//		System.out.println(marks);
+//		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+	}
+}
+
+//	128
+//	127
+//	-128
+
+
---
+++ java/SecondProgram.java
@@ -0,0 +1,12 @@
+public class SecondProgram
+{
+	public static void main(String []args)
+	{
+		char c='9';
+		int i=c-48;			//implicit casting from char to int
+		System.out.println(i);
+	}
+}
+
+
+
