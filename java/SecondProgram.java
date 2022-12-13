b74e40a836c3bc4d42418a47e8700491bbaa9dba
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2022-12-13 15:04:33 +0530

    3pm

---
+++ java/SecondProgram.java
@@ -0,0 +1,17 @@
+public class Student
+{
+	String name;
+	int marks;
+
+	public static void main(String []args)
+	{
+		Student raja=new Student();
+		int mark=20;
+		char c='9';
+		int i=c-48;			//implicit casting from char to int
+		System.out.println(i);
+	}
+}
+
+
+
