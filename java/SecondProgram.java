aa5a00ae65701fbe9494c32628b2a79e677bc9e1
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2022-12-13 12:52:02 +0530

    12:51

--- java/SecondProgram.java
+++ java/SecondProgram.java
@@ -1,7 +1,12 @@
-public class SecondProgram
+public class Student
 {
+	String name;
+	int marks;
+
 	public static void main(String []args)
 	{
+		Student raja=new Student();
+		int mark=20;
 		char c='9';
 		int i=c-48;			//implicit casting from char to int
 		System.out.println(i);
---
+++ java/dec-13-challenge-1/src/IfDemo.java
@@ -0,0 +1,15 @@
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int i=50;
+		if(i<30)
+		{	
+			i++;
+		}else
+		{	
+			System.out.println(i);
+		}
+	}
+
+}
