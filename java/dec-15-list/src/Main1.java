af04284681682b5de7ecc3206cacc430958be922
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2022-12-15 10:35:06 +0530

    11am

---
+++ java/dec-15-list/src/Main1.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> marks=new ArrayList<>();
+		marks.add(90);
+		marks.add(50);
+		marks.add(40);
+		marks.add(50);
+		marks.add(80);
+		marks.add(90);
+		marks.add(100);
+		marks.add(45);
+		marks.add(76);
+		
+//		System.out.println(marks);
+		
+//		for(int i=0;i<marks.size();i++)
+//		{
+//			//Vector and ArrayList only have index
+//			System.out.println(marks.get(i));
+//		}
+		
+//		for(Integer x:marks)
+//			System.out.println(x);
+		
+//		Iterator<Integer> it = marks.iterator();
+//		while(it.hasNext())
+//		{
+//			Integer x = it.next();
+//			System.out.println(x);
+//		}
+	}
+
+}
