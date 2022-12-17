b9f94929260e3be8622d599483645b948b45b5d1
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2022-12-17 17:56:51 +0530

    6pm

--- java/dec-17-1/src/MainPrecision.java
+++ java/dec-17-1/src/MainPrecision.java
@@ -5,15 +5,20 @@ import java.util.List;
 public class MainPrecision {
 
 	public static void main(String[] args) {
-//		String result=String.format("%d",(int) 6.6);
-//		System.out.println(result);
-		List<Integer> source=Arrays.asList(1,2,3,4,5,6,7);
-		
-		Collections.replaceAll(source, 2, 3);
-//		Collections.reverse(source);
-		System.out.println(source);
-		
+////		String result=String.format("%d",(int) 6.6);
+////		System.out.println(result);
+//		List<Integer> source=Arrays.asList(1,2,3,4,5,6,7);
+//		
+//		Collections.replaceAll(source, 2, 3);
+////		Collections.reverse(source);
+//		System.out.println(source);
+//		
 
+		String string1="helloworld";
+		String string2="world";
+		
+		string1=string1.replace(string2, "");
+		System.out.println(string1);
 	}
 
 }
