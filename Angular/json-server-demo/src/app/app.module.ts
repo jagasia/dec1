c9bbe2c957818b09849599af87a4dfd364235156
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2022-12-27 11:47:59 +0530

    27Dec11:47am

--- Angular/json-server-demo/src/app/app.module.ts
+++ Angular/json-server-demo/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductComponent } from './product/product.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
